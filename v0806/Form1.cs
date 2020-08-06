52e7c263521664628ae2f8db7c8cc237278c012a
Yu Tanaka <YRK00337@nifty.com>
2020-08-06 09:31:33 +0900

    完成

--- v0806/Form1.cs
+++ v0806/Form1.cs
@@ -12,9 +12,29 @@ namespace v0806
 {
     public partial class Form1 : Form
     {
+        int vx = -10;
+        int vy = -10;
+        string ws = "(・◇・)";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string tmp = label1.Text;
+            label1.Text = ws;
+            ws = tmp;
+
+            label1.Left += vx;
+            label1.Top += vy;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            vx = -vx;
+            vy = -vy;
         }
     }
 }
